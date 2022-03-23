f24b36589f2bcaa17ad5cb2753f2c4fbd328c6b4
Kyle Peacock <kylpeacock@gmail.com>
2022-03-23 15:11:03 -0700

    Initial commit.

---
+++ src/parcel_dfx_assets/src/index.js
@@ -0,0 +1,19 @@
+import { parcel_dfx } from "../../declarations/parcel_dfx";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await parcel_dfx.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
